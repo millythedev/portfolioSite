c84dc0840dcd71fb9a0f8544d6a3cb586e8a7bca
millythedev <emilio.cleveland123@gmail.com>
2025-05-03 11:24:02 -0500

    Fix module script loading with .mjs extension

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,8 @@ export default defineConfig({
         manualChunks: undefined,
         assetFileNames: 'assets/[name].[hash][extname]',
         chunkFileNames: 'assets/[name].[hash].js',
-        entryFileNames: 'assets/[name].[hash].js'
+        entryFileNames: 'assets/[name].[hash].js',
+        format: 'es'
       }
     }
   },

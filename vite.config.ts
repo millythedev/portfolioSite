583442ebf23c7fd454e7f7c7c23f4a7eec6a915f
millythedev <emilio.cleveland123@gmail.com>
2025-05-06 15:33:24 -0500

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolioSite/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolioSite/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
